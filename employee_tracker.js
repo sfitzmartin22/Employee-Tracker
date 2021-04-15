01b2fa2d00ce7af59813a3eabc30bd99ad4b6aa0
Sean Fitzmartin <sfitzmartin22@gmail.com>
2021-04-15 16:52:55 -0400

    setup the init function which provides the initial framework for how inquirer is going to ask the quesions to flow through the node.  my next commits will be focused on creating the functions that will do the legwork for adding, viewing and updating the databse

--- employee_tracker.js
+++ employee_tracker.js
@@ -11,12 +11,72 @@ const connection = mysql.createConnection({
   user: 'root',
 
   // Be sure to update with your own MySQL password!
-  password: '',
+  password: '22seanfitz22',
   database: 'employee_trackerDB',
 });
 
 connection.connect((err) => {
   if (err) throw err;
-  runSearch();
+ init(); 
 });
+
+ const init = () => {
+  inquirer
+    .prompt({
+      name: 'action',
+      type: 'rawlist',
+      message: 'What would you like to do?',
+      choices: [
+        'add departments',
+        'add roles',
+        'add employees',
+        'view departments',
+        'view roles',
+        'view employees',
+        'update employee roles',
+      ],
+    })
+    .then((answer) => {
+      switch (answer.action) {
+        case 'add departments':
+         // artistSearch();
+         console.log("add department");
+          break;
+
+        case 'add roles':
+         // multiSearch();
+         console.log("add roles");
+          break;
+
+        case 'add employees':
+         // rangeSearch();
+         console.log("add employees");
+         break;
+
+        case 'view departments':
+        // songSearch();
+        console.log("view departments");
+          break;
+
+        case 'view roles':
+         // songAndAlbumSearch();
+         console.log("view roles");
+          break;
+
+        case 'view employees':
+        //  rangeSearch();
+        console.log("view employees");
+          break;
+
+        case 'update employee roles':
+        //  rangeSearch();
+        console.log("update employee roles");
+          break;
+
+        default:
+          console.log(`Invalid action: ${answer.action}`);
+          break;
+      }
+    });
+};
 
